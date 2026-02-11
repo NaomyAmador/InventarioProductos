84b21443fdd1819dc23c907b02be54566ed4fe41
naomyamador_04 <naomyabigail2009@gmail.com>
2026-02-10 21:53:13 -0400

    Diseño del Formulario hecho

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -68,195 +68,244 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(19, 11);
+            label1.Font = new Font("Century Gothic", 7.20000029F, FontStyle.Bold);
+            label1.ForeColor = Color.PaleVioletRed;
+            label1.Location = new Point(20, 15);
             label1.Name = "label1";
-            label1.Size = new Size(198, 20);
+            label1.Size = new Size(192, 16);
             label1.TabIndex = 1;
             label1.Text = "Conexión con Base de Datos";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(22, 36);
+            label2.Font = new Font("Century Gothic", 7.20000029F, FontStyle.Bold);
+            label2.ForeColor = Color.PaleVioletRed;
+            label2.Location = new Point(20, 31);
             label2.Name = "label2";
-            label2.Size = new Size(134, 20);
+            label2.Size = new Size(129, 16);
             label2.TabIndex = 2;
             label2.Text = "y Entity Framework";
             // 
             // Lbl_Id
             // 
             Lbl_Id.AutoSize = true;
+            Lbl_Id.Font = new Font("Century Gothic", 9F, FontStyle.Bold);
+            Lbl_Id.ForeColor = Color.LightCoral;
             Lbl_Id.Location = new Point(20, 117);
             Lbl_Id.Name = "Lbl_Id";
-            Lbl_Id.Size = new Size(22, 20);
+            Lbl_Id.Size = new Size(22, 18);
             Lbl_Id.TabIndex = 3;
             Lbl_Id.Text = "Id";
             // 
             // Lbl_Producto
             // 
             Lbl_Producto.AutoSize = true;
+            Lbl_Producto.Font = new Font("Century Gothic", 9F, FontStyle.Bold);
+            Lbl_Producto.ForeColor = Color.LightCoral;
             Lbl_Producto.Location = new Point(17, 184);
             Lbl_Producto.Name = "Lbl_Producto";
-            Lbl_Producto.Size = new Size(69, 20);
+            Lbl_Producto.Size = new Size(74, 18);
             Lbl_Producto.TabIndex = 4;
             Lbl_Producto.Text = "Producto";
             // 
             // Lbl_Precio
             // 
             Lbl_Precio.AutoSize = true;
+            Lbl_Precio.Font = new Font("Century Gothic", 9F, FontStyle.Bold);
+            Lbl_Precio.ForeColor = Color.LightCoral;
             Lbl_Precio.Location = new Point(19, 250);
             Lbl_Precio.Name = "Lbl_Precio";
-            Lbl_Precio.Size = new Size(50, 20);
+            Lbl_Precio.Size = new Size(55, 18);
             Lbl_Precio.TabIndex = 5;
             Lbl_Precio.Text = "Precio";
             // 
             // Lbl_Stop
             // 
             Lbl_Stop.AutoSize = true;
+            Lbl_Stop.Font = new Font("Century Gothic", 9F, FontStyle.Bold);
+            Lbl_Stop.ForeColor = Color.LightCoral;
             Lbl_Stop.Location = new Point(21, 313);
             Lbl_Stop.Name = "Lbl_Stop";
-            Lbl_Stop.Size = new Size(40, 20);
+            Lbl_Stop.Size = new Size(40, 18);
             Lbl_Stop.TabIndex = 6;
             Lbl_Stop.Text = "Stop";
             // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(22, 67);
+            pictureBox1.Image = Properties.Resources.download__15_;
+            pictureBox1.Location = new Point(22, 58);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(195, 35);
+            pictureBox1.Size = new Size(190, 38);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 7;
             pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
-            pictureBox2.Location = new Point(668, 13);
+            pictureBox2.Image = Properties.Resources.download__11_;
+            pictureBox2.Location = new Point(665, 13);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(162, 114);
+            pictureBox2.Size = new Size(165, 114);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox2.TabIndex = 8;
             pictureBox2.TabStop = false;
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(351, 13);
+            label7.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.Crimson;
+            label7.Location = new Point(254, 19);
             label7.Name = "label7";
-            label7.Size = new Size(311, 20);
+            label7.Size = new Size(405, 23);
             label7.TabIndex = 9;
             label7.Text = "°˖✧ ━━━━━━ ✿ Fragance Market ✿ ━━━━━━ ✧˖°";
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(338, 42);
+            label8.Font = new Font("Century Gothic", 7.20000029F);
+            label8.Location = new Point(297, 42);
             label8.Name = "label8";
-            label8.Size = new Size(324, 20);
+            label8.Size = new Size(320, 17);
             label8.TabIndex = 10;
             label8.Text = "Lugar en donde la naturaleza es la Protagonista";
             // 
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(300, 98);
+            label9.Font = new Font("Century Gothic", 7.20000029F);
+            label9.Location = new Point(291, 98);
             label9.Name = "label9";
-            label9.Size = new Size(362, 20);
+            label9.Size = new Size(359, 17);
             label9.TabIndex = 12;
             label9.Text = "Cada acción realizada se registra en la Base de Datos";
             // 
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(392, 78);
+            label10.Font = new Font("Century Gothic", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.ForeColor = Color.PaleVioletRed;
+            label10.Location = new Point(300, 73);
             label10.Name = "label10";
-            label10.Size = new Size(270, 20);
+            label10.Size = new Size(343, 22);
             label10.TabIndex = 11;
             label10.Text = "◦◦,`°.✽✦✽.¡Elige tu Pedido Ya!.✽✦✽.°`,◦◦";
             // 
             // label11
             // 
             label11.AutoSize = true;
-            label11.Location = new Point(265, 146);
+            label11.Font = new Font("Century Gothic", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.ForeColor = Color.MediumVioletRed;
+            label11.Location = new Point(216, 145);
             label11.Name = "label11";
-            label11.Size = new Size(565, 20);
+            label11.Size = new Size(616, 18);
             label11.TabIndex = 13;
             label11.Text = "❀ Tabla Productos - Base de Datos InventarioProductosBDD y Operaciones CRUD ❀";
             // 
             // TxtBox_Id
             // 
+            TxtBox_Id.Font = new Font("Century Gothic", 9F);
+            TxtBox_Id.ForeColor = Color.Gray;
             TxtBox_Id.Location = new Point(22, 145);
             TxtBox_Id.Name = "TxtBox_Id";
-            TxtBox_Id.Size = new Size(125, 27);
+            TxtBox_Id.Size = new Size(157, 26);
             TxtBox_Id.TabIndex = 14;
+            TxtBox_Id.Text = "Para crear → no Id";
             // 
             // TxtBox_Producto
             // 
+            TxtBox_Producto.Font = new Font("Century Gothic", 9F);
+            TxtBox_Producto.ForeColor = Color.Gray;
             TxtBox_Producto.Location = new Point(22, 213);
             TxtBox_Producto.Name = "TxtBox_Producto";
-            TxtBox_Producto.Size = new Size(125, 27);
+            TxtBox_Producto.Size = new Size(157, 26);
             TxtBox_Producto.TabIndex = 15;
             // 
             // TxtBox_Precio
             // 
+            TxtBox_Precio.Font = new Font("Century Gothic", 9F);
+            TxtBox_Precio.ForeColor = Color.Gray;
             TxtBox_Precio.Location = new Point(22, 277);
             TxtBox_Precio.Name = "TxtBox_Precio";
-            TxtBox_Precio.Size = new Size(125, 27);
+            TxtBox_Precio.Size = new Size(157, 26);
             TxtBox_Precio.TabIndex = 16;
             // 
             // TxtBox_Stop
             // 
+            TxtBox_Stop.Font = new Font("Century Gothic", 9F);
+            TxtBox_Stop.ForeColor = Color.Gray;
             TxtBox_Stop.Location = new Point(22, 336);
             TxtBox_Stop.Name = "TxtBox_Stop";
-            TxtBox_Stop.Size = new Size(125, 27);
+            TxtBox_Stop.Size = new Size(157, 26);
             TxtBox_Stop.TabIndex = 17;
             // 
             // Btn_Crear
             // 
-            Btn_Crear.Location = new Point(225, 194);
+            Btn_Crear.BackColor = Color.MediumVioletRed;
+            Btn_Crear.Font = new Font("Century Gothic", 7.8F, FontStyle.Bold);
+            Btn_Crear.ForeColor = SystemColors.ButtonHighlight;
+            Btn_Crear.Location = new Point(216, 179);
             Btn_Crear.Name = "Btn_Crear";
-            Btn_Crear.Size = new Size(94, 29);
+            Btn_Crear.Size = new Size(105, 37);
             Btn_Crear.TabIndex = 18;
             Btn_Crear.Text = "Crear";
-            Btn_Crear.UseVisualStyleBackColor = true;
+            Btn_Crear.UseVisualStyleBackColor = false;
             // 
             // Btn_Eliminar
             // 
-            Btn_Eliminar.Location = new Point(225, 334);
+            Btn_Eliminar.BackColor = Color.MediumVioletRed;
+            Btn_Eliminar.Font = new Font("Century Gothic", 7.8F, FontStyle.Bold);
+            Btn_Eliminar.ForeColor = SystemColors.ButtonHighlight;
+            Btn_Eliminar.Location = new Point(216, 334);
             Btn_Eliminar.Name = "Btn_Eliminar";
-            Btn_Eliminar.Size = new Size(94, 29);
+            Btn_Eliminar.Size = new Size(105, 40);
             Btn_Eliminar.TabIndex = 19;
             Btn_Eliminar.Text = "Eliminar";
-            Btn_Eliminar.UseVisualStyleBackColor = true;
+            Btn_Eliminar.UseVisualStyleBackColor = false;
             // 
             // Btn_Actualizar
             // 
-            Btn_Actualizar.Location = new Point(225, 299);
+            Btn_Actualizar.BackColor = Color.MediumVioletRed;
+            Btn_Actualizar.Font = new Font("Century Gothic", 7.8F, FontStyle.Bold);
+            Btn_Actualizar.ForeColor = SystemColors.ButtonHighlight;
+            Btn_Actualizar.Location = new Point(216, 292);
             Btn_Actualizar.Name = "Btn_Actualizar";
-            Btn_Actualizar.Size = new Size(94, 29);
+            Btn_Actualizar.Size = new Size(105, 40);
             Btn_Actualizar.TabIndex = 20;
             Btn_Actualizar.Text = "Actualizar";
-            Btn_Actualizar.UseVisualStyleBackColor = true;
+            Btn_Actualizar.UseVisualStyleBackColor = false;
             // 
             // Btn_MostrarTodo
             // 
-            Btn_MostrarTodo.Location = new Point(225, 264);
+            Btn_MostrarTodo.BackColor = Color.MediumVioletRed;
+            Btn_MostrarTodo.Font = new Font("Century Gothic", 7.8F, FontStyle.Bold);
+            Btn_MostrarTodo.ForeColor = SystemColors.ButtonHighlight;
+            Btn_MostrarTodo.Location = new Point(216, 253);
             Btn_MostrarTodo.Name = "Btn_MostrarTodo";
-            Btn_MostrarTodo.Size = new Size(94, 29);
+            Btn_MostrarTodo.Size = new Size(105, 38);
             Btn_MostrarTodo.TabIndex = 21;
             Btn_MostrarTodo.Text = "Mostrar Todo";
-            Btn_MostrarTodo.UseVisualStyleBackColor = true;
+            Btn_MostrarTodo.UseVisualStyleBackColor = false;
             // 
             // Btn_Buscar
             // 
-            Btn_Buscar.Location = new Point(225, 229);
+            Btn_Buscar.BackColor = Color.MediumVioletRed;
+            Btn_Buscar.Font = new Font("Century Gothic", 7.8F, FontStyle.Bold);
+            Btn_Buscar.ForeColor = SystemColors.ButtonHighlight;
+            Btn_Buscar.Location = new Point(216, 216);
             Btn_Buscar.Name = "Btn_Buscar";
-            Btn_Buscar.Size = new Size(94, 29);
+            Btn_Buscar.Size = new Size(105, 36);
             Btn_Buscar.TabIndex = 22;
             Btn_Buscar.Text = "Buscar";
-            Btn_Buscar.UseVisualStyleBackColor = true;
+            Btn_Buscar.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LavenderBlush;
             ClientSize = new Size(842, 389);
             Controls.Add(Btn_Buscar);
             Controls.Add(Btn_MostrarTodo);
