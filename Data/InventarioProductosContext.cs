c4efc002093b72e006fc4fa5f12fcdbcd51d23a6
naomyamador_04 <naomyabigail2009@gmail.com>
2026-02-10 21:02:27 -0400

    DbContext hecho correctamente

---
+++ Data/InventarioProductosContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventarioProductos_Práctica_Entity_Framework.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventarioProductos_Práctica_Entity_Framework.Data
+{
+    public class InventarioProductosContext : DbContext
+    {
+            public DbSet<Productos> Productos { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder options)
+            {
+                options.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=InventarioProductosBDD;Trusted_Connection=True;TrustServerCertificate=True;");
+            }
+    }
+}
