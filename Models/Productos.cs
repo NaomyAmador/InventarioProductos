25366769cddbae8a8ae0df137487281d797ab7e3
naomyamador_04 <naomyabigail2009@gmail.com>
2026-02-10 20:57:21 -0400

    Clase Productos creada

---
+++ Models/Productos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace InventarioProductos_Práctica_Entity_Framework.Models
+{
+    public class Productos
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Producto { get; set; }
+        public decimal Precio { get; set; }
+        public int Stop { get; set; }
+    }
+}
