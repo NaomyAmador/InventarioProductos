9b83ff72b8390f63b0dbe675f9cb2340764eebb0
naomyamador_04 <naomyabigail2009@gmail.com>
2026-02-10 20:49:40 -0400

    Archivo .gitignore configurado

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace InventarioProductos_Práctica_Entity_Framework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
