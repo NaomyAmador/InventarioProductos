f7bbf1e45ac2372787999c4675419682f84c9606
naomyamador_04 <naomyabigail2009@gmail.com>
2026-02-10 21:34:04 -0400

    Interfaz gráfica base creada

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,292 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            DataGridView_TablaBDD = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            Lbl_Id = new Label();
+            Lbl_Producto = new Label();
+            Lbl_Precio = new Label();
+            Lbl_Stop = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            TxtBox_Id = new TextBox();
+            TxtBox_Producto = new TextBox();
+            TxtBox_Precio = new TextBox();
+            TxtBox_Stop = new TextBox();
+            Btn_Crear = new Button();
+            Btn_Eliminar = new Button();
+            Btn_Actualizar = new Button();
+            Btn_MostrarTodo = new Button();
+            Btn_Buscar = new Button();
+            ((System.ComponentModel.ISupportInitialize)DataGridView_TablaBDD).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // DataGridView_TablaBDD
+            // 
+            DataGridView_TablaBDD.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DataGridView_TablaBDD.Location = new Point(337, 178);
+            DataGridView_TablaBDD.Name = "DataGridView_TablaBDD";
+            DataGridView_TablaBDD.RowHeadersWidth = 51;
+            DataGridView_TablaBDD.Size = new Size(493, 199);
+            DataGridView_TablaBDD.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(19, 11);
+            label1.Name = "label1";
+            label1.Size = new Size(198, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Conexión con Base de Datos";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(22, 36);
+            label2.Name = "label2";
+            label2.Size = new Size(134, 20);
+            label2.TabIndex = 2;
+            label2.Text = "y Entity Framework";
+            // 
+            // Lbl_Id
+            // 
+            Lbl_Id.AutoSize = true;
+            Lbl_Id.Location = new Point(20, 117);
+            Lbl_Id.Name = "Lbl_Id";
+            Lbl_Id.Size = new Size(22, 20);
+            Lbl_Id.TabIndex = 3;
+            Lbl_Id.Text = "Id";
+            // 
+            // Lbl_Producto
+            // 
+            Lbl_Producto.AutoSize = true;
+            Lbl_Producto.Location = new Point(17, 184);
+            Lbl_Producto.Name = "Lbl_Producto";
+            Lbl_Producto.Size = new Size(69, 20);
+            Lbl_Producto.TabIndex = 4;
+            Lbl_Producto.Text = "Producto";
+            // 
+            // Lbl_Precio
+            // 
+            Lbl_Precio.AutoSize = true;
+            Lbl_Precio.Location = new Point(19, 250);
+            Lbl_Precio.Name = "Lbl_Precio";
+            Lbl_Precio.Size = new Size(50, 20);
+            Lbl_Precio.TabIndex = 5;
+            Lbl_Precio.Text = "Precio";
+            // 
+            // Lbl_Stop
+            // 
+            Lbl_Stop.AutoSize = true;
+            Lbl_Stop.Location = new Point(21, 313);
+            Lbl_Stop.Name = "Lbl_Stop";
+            Lbl_Stop.Size = new Size(40, 20);
+            Lbl_Stop.TabIndex = 6;
+            Lbl_Stop.Text = "Stop";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(22, 67);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(195, 35);
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(668, 13);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(162, 114);
+            pictureBox2.TabIndex = 8;
+            pictureBox2.TabStop = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(351, 13);
+            label7.Name = "label7";
+            label7.Size = new Size(311, 20);
+            label7.TabIndex = 9;
+            label7.Text = "°˖✧ ━━━━━━ ✿ Fragance Market ✿ ━━━━━━ ✧˖°";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(338, 42);
+            label8.Name = "label8";
+            label8.Size = new Size(324, 20);
+            label8.TabIndex = 10;
+            label8.Text = "Lugar en donde la naturaleza es la Protagonista";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(300, 98);
+            label9.Name = "label9";
+            label9.Size = new Size(362, 20);
+            label9.TabIndex = 12;
+            label9.Text = "Cada acción realizada se registra en la Base de Datos";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(392, 78);
+            label10.Name = "label10";
+            label10.Size = new Size(270, 20);
+            label10.TabIndex = 11;
+            label10.Text = "◦◦,`°.✽✦✽.¡Elige tu Pedido Ya!.✽✦✽.°`,◦◦";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(265, 146);
+            label11.Name = "label11";
+            label11.Size = new Size(565, 20);
+            label11.TabIndex = 13;
+            label11.Text = "❀ Tabla Productos - Base de Datos InventarioProductosBDD y Operaciones CRUD ❀";
+            // 
+            // TxtBox_Id
+            // 
+            TxtBox_Id.Location = new Point(22, 145);
+            TxtBox_Id.Name = "TxtBox_Id";
+            TxtBox_Id.Size = new Size(125, 27);
+            TxtBox_Id.TabIndex = 14;
+            // 
+            // TxtBox_Producto
+            // 
+            TxtBox_Producto.Location = new Point(22, 213);
+            TxtBox_Producto.Name = "TxtBox_Producto";
+            TxtBox_Producto.Size = new Size(125, 27);
+            TxtBox_Producto.TabIndex = 15;
+            // 
+            // TxtBox_Precio
+            // 
+            TxtBox_Precio.Location = new Point(22, 277);
+            TxtBox_Precio.Name = "TxtBox_Precio";
+            TxtBox_Precio.Size = new Size(125, 27);
+            TxtBox_Precio.TabIndex = 16;
+            // 
+            // TxtBox_Stop
+            // 
+            TxtBox_Stop.Location = new Point(22, 336);
+            TxtBox_Stop.Name = "TxtBox_Stop";
+            TxtBox_Stop.Size = new Size(125, 27);
+            TxtBox_Stop.TabIndex = 17;
+            // 
+            // Btn_Crear
+            // 
+            Btn_Crear.Location = new Point(225, 194);
+            Btn_Crear.Name = "Btn_Crear";
+            Btn_Crear.Size = new Size(94, 29);
+            Btn_Crear.TabIndex = 18;
+            Btn_Crear.Text = "Crear";
+            Btn_Crear.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Eliminar
+            // 
+            Btn_Eliminar.Location = new Point(225, 334);
+            Btn_Eliminar.Name = "Btn_Eliminar";
+            Btn_Eliminar.Size = new Size(94, 29);
+            Btn_Eliminar.TabIndex = 19;
+            Btn_Eliminar.Text = "Eliminar";
+            Btn_Eliminar.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Actualizar
+            // 
+            Btn_Actualizar.Location = new Point(225, 299);
+            Btn_Actualizar.Name = "Btn_Actualizar";
+            Btn_Actualizar.Size = new Size(94, 29);
+            Btn_Actualizar.TabIndex = 20;
+            Btn_Actualizar.Text = "Actualizar";
+            Btn_Actualizar.UseVisualStyleBackColor = true;
+            // 
+            // Btn_MostrarTodo
+            // 
+            Btn_MostrarTodo.Location = new Point(225, 264);
+            Btn_MostrarTodo.Name = "Btn_MostrarTodo";
+            Btn_MostrarTodo.Size = new Size(94, 29);
+            Btn_MostrarTodo.TabIndex = 21;
+            Btn_MostrarTodo.Text = "Mostrar Todo";
+            Btn_MostrarTodo.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Buscar
+            // 
+            Btn_Buscar.Location = new Point(225, 229);
+            Btn_Buscar.Name = "Btn_Buscar";
+            Btn_Buscar.Size = new Size(94, 29);
+            Btn_Buscar.TabIndex = 22;
+            Btn_Buscar.Text = "Buscar";
+            Btn_Buscar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(842, 389);
+            Controls.Add(Btn_Buscar);
+            Controls.Add(Btn_MostrarTodo);
+            Controls.Add(Btn_Actualizar);
+            Controls.Add(Btn_Eliminar);
+            Controls.Add(Btn_Crear);
+            Controls.Add(TxtBox_Stop);
+            Controls.Add(TxtBox_Precio);
+            Controls.Add(TxtBox_Producto);
+            Controls.Add(TxtBox_Id);
+            Controls.Add(label11);
+            Controls.Add(label9);
+            Controls.Add(label10);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(Lbl_Stop);
+            Controls.Add(Lbl_Precio);
+            Controls.Add(Lbl_Producto);
+            Controls.Add(Lbl_Id);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(DataGridView_TablaBDD);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)DataGridView_TablaBDD).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView DataGridView_TablaBDD;
+        private Label label1;
+        private Label label2;
+        private Label Lbl_Id;
+        private Label Lbl_Producto;
+        private Label Lbl_Precio;
+        private Label Lbl_Stop;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private TextBox TxtBox_Id;
+        private TextBox TxtBox_Producto;
+        private TextBox TxtBox_Precio;
+        private TextBox TxtBox_Stop;
+        private Button Btn_Crear;
+        private Button Btn_Eliminar;
+        private Button Btn_Actualizar;
+        private Button Btn_MostrarTodo;
+        private Button Btn_Buscar;
     }
 }
